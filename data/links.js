cf9295cb12e6746fe3e0f4ef6a2fbadb0fca6148
Alexander <kaamosdao@gmail.com>
2023-07-25 21:45:29 +0300

    Add data: add data for mapping

---
+++ data/links.js
@@ -0,0 +1,8 @@
+const links = [
+  { path: 'tours', title: 'Туры' },
+  { path: 'create', title: 'Создать тур' },
+  { path: 'feedback', title: 'Отзывы' },
+  { path: 'stories', title: 'Истории' },
+];
+
+export default links;
---
+++ data/navigation.js
@@ -0,0 +1,29 @@
+const navigation = [
+  {
+    link: '/',
+    name: 'Популярные',
+    current: true,
+  },
+  {
+    link: '/',
+    name: 'Авторские',
+    current: false,
+  },
+  {
+    link: '/',
+    name: 'Походы',
+    current: false,
+  },
+  {
+    link: '/',
+    name: 'Сплавы',
+    current: false,
+  },
+  {
+    link: '/',
+    name: 'Велопрогулки',
+    current: false,
+  },
+];
+
+export default navigation;
