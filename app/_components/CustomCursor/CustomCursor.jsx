3c1a4221d3037afea9006144bd0a0e0202ef1f53
Alexander <kaamosdao@gmail.com>
2023-07-28 16:16:45 +0300

    Refactor CustomCursor: add transition logic, refactor animations

--- app/_components/CustomCursor/CustomCursor.jsx
+++ app/_components/CustomCursor/CustomCursor.jsx
@@ -1,37 +1,51 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { gsap } from 'gsap';
+import { EaselPlugin } from 'gsap/EaselPlugin';
+import { TextPlugin } from 'gsap/TextPlugin';
+import { Transition } from 'react-transition-group';
 
-import selectStuck from '@/store/selectors/cursorSelectors';
-import useCursorRef from '@/hooks/useCursorRef';
-import useCursorFollowerRef from '@/hooks/useCursorFollowerRef';
+import selectStuckData, {
+  selectCursorState,
+} from '@/store/selectors/cursorSelectors';
+
+import { getMoveX, getMoveY } from '@/utils/getMoveCoordinate';
+import cursorStateType from '@/utils/types';
+import CursorAnimation from '@/utils/CursorAnimation';
 
 import s from './CustomCursor.module.scss';
 
-const getMoveX = (cursor, duration, ease = 'power3') =>
-  gsap.quickTo(cursor, 'x', { duration, ease });
-const getMoveY = (cursor, duration, ease = 'power3') =>
-  gsap.quickTo(cursor, 'y', { duration, ease });
-
 const CustomCursor = () => {
-  const cursorRef = useCursorRef();
-  const cursorFollowerRef = useCursorFollowerRef();
+  const cursor = useRef(null);
+  const follower = useRef(null);
+  const animation = useRef(null);
 
-  const isStuck = useSelector(selectStuck);
+  const cursorState = useSelector(selectCursorState);
+  const stuckCursorData = useSelector(selectStuckData);
+
+  const [inTransition, setInTransition] = useState(false);
 
   useEffect(() => {
-    const moveXFollower = getMoveX(cursorFollowerRef.current, 0.6);
-    const moveYFollower = getMoveY(cursorFollowerRef.current, 0.6);
+    gsap.registerPlugin(EaselPlugin, TextPlugin);
+    animation.current = new CursorAnimation(cursor, follower);
+  }, []);
+
+  useEffect(() => {
+    setInTransition((prev) => !prev);
+  }, [cursorState]);
+
+  useEffect(() => {
+    const moveXFollower = getMoveX(follower.current, 0.6);
+    const moveYFollower = getMoveY(follower.current, 0.6);
 
     const moveCursor = (e) => {
       let xFollower;
       let yFollower;
 
-      if (isStuck) {
-        const linkItem = e.target;
-        const { left, top, width, height } = linkItem.getBoundingClientRect();
+      if (stuckCursorData) {
+        const { left, top, width, height } = stuckCursorData;
 
         xFollower = Math.round(left + width / 2);
         yFollower = Math.round(top + height / 2);
@@ -40,7 +54,7 @@ const CustomCursor = () => {
         yFollower = e.clientY;
       }
 
-      gsap.to(cursorRef.current, {
+      gsap.to(cursor.current, {
         x: e.clientX,
         y: e.clientY,
         duration: 0.0,
@@ -53,16 +67,57 @@ const CustomCursor = () => {
     window.addEventListener('mousemove', moveCursor);
 
     return () => window.removeEventListener('mousemove', moveCursor);
-  }, [isStuck, cursorRef, cursorFollowerRef]);
+  }, [stuckCursorData, cursor, follower]);
+
+  const onEnter = () => {
+    animation.current.reset();
+  };
+
+  const onEntering = () => {
+    switch (cursorState) {
+      case cursorStateType.pulse:
+        animation.current.pulse();
+        break;
+
+      case cursorStateType.stuck:
+        animation.current.stuck(stuckCursorData);
+        break;
+
+      case cursorStateType.growDot:
+        animation.current.growDot();
+        break;
+
+      case cursorStateType.text:
+        animation.current.text();
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const onExit = onEnter;
+  const onExiting = onEntering;
 
   return (
-    <>
-      <div ref={cursorRef} className={s.cursor} id="cursor" />
-      <div ref={cursorFollowerRef} className={s.follower} id="follower">
-        <div className={s.circle} id="circle" />
-        <div className={s.text} id="text" />
-      </div>
-    </>
+    <Transition
+      in={inTransition}
+      timeout={300}
+      onEnter={onEnter}
+      onExit={onExit}
+      onEntering={onEntering}
+      onExiting={onExiting}
+    >
+      {() => (
+        <>
+          <div ref={cursor} className={s.cursor} id="cursor" />
+          <div ref={follower} className={s.follower} id="follower">
+            <div className={s.circle} id="circle" />
+            <div className={s.text} id="text" />
+          </div>
+        </>
+      )}
+    </Transition>
   );
 };
 
