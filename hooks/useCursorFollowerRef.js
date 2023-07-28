85f305c21a644e908d804c918e359e1dae1b44db
Alexander <kaamosdao@gmail.com>
2023-07-28 16:13:39 +0300

    Change app: remove cursorRefContext, CursorFollowerRefContext

--- hooks/useCursorFollowerRef.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import CursorFollowerRefContext from '../context/CursorFollowerRefContext.jsx';
-
-const useCursorFollowerRef = () => useContext(CursorFollowerRefContext);
-
-export default useCursorFollowerRef;
--- hooks/useCursorRef.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import CursorRefContext from '../context/CursorRefContext.jsx';
-
-const useCursorRef = () => useContext(CursorRefContext);
-
-export default useCursorRef;
