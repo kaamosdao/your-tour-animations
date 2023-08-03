f9608d845d5defa6cc914ccab5c369d5e7b0b753
Alexander <kaamosdao@gmail.com>
2023-08-03 12:41:17 +0300

    Refactor SectionContact: add HoverCursor

--- app/_components/SectionContact/SectionContact.jsx
+++ app/_components/SectionContact/SectionContact.jsx
@@ -1,69 +1,49 @@
 'use client';
 
-import { useDispatch } from 'react-redux';
-
-import { setCursor } from '@/store/slices/cursorSlice';
-
-import cursorState from '@/utils/types';
-
 import Picture from '../Picture';
+import HoverCursor from '../CustomCursor/HoverCursor';
 
 import s from './SectionContact.module.scss';
 
-const SectionContact = () => {
-  const dispatch = useDispatch();
-
-  const onMouseEnter = () => {
-    dispatch(setCursor(cursorState.pulse));
-  };
-
-  const onMouseLeave = () => {
-    dispatch(setCursor(cursorState.default));
-  };
-
-  return (
-    <section className={s.contact}>
-      <h2 className={s.title}>
-        Пора в путешествие
-        <br />
-        вместе с нами!
-      </h2>
-      <p className={s.text}>
-        Напиши на почту и узнай подробности
-        <br />
-        на&nbsp;
-        <a
-          className={s.link}
-          href="mailto:yourtour@gmail.com"
-          onMouseEnter={onMouseEnter}
-          onMouseLeave={onMouseLeave}
-        >
+const SectionContact = () => (
+  <section className={s.contact}>
+    <h2 className={s.title}>
+      Пора в путешествие
+      <br />
+      вместе с нами!
+    </h2>
+    <p className={s.text}>
+      Напиши на почту и узнай подробности
+      <br />
+      на&nbsp;
+      <HoverCursor cursorType="pulse">
+        <a className={s.link} href="mailto:yourtour@gmail.com">
           yourtour@gmail.com
         </a>
-      </p>
-      <div className={s.picture}>
-        <Picture
-          desktopImg={{
-            width: 370,
-            height: 370,
-            url: '/img/histories/icon-village-desktop-lg',
-          }}
-          tabletImg={{
-            width: 370,
-            height: 370,
-            url: '/img/histories/icon-village-desktop-md',
-          }}
-          defaultImg={{
-            width: 133,
-            height: 130,
-            url: '/img/histories/icon-village-mobile',
-          }}
-          alt="Деревня у озера в горах"
-          format="png"
-        />
-      </div>
-    </section>
-  );
-};
+      </HoverCursor>
+    </p>
+    <div className={s.picture}>
+      <Picture
+        desktopImg={{
+          width: 370,
+          height: 370,
+          url: '/img/histories/icon-village-desktop-lg',
+        }}
+        tabletImg={{
+          width: 370,
+          height: 370,
+          url: '/img/histories/icon-village-desktop-md',
+        }}
+        defaultImg={{
+          width: 133,
+          height: 130,
+          url: '/img/histories/icon-village-mobile',
+        }}
+        alt="Деревня у озера в горах"
+        format="png"
+      />
+    </div>
+  </section>
+);
 
 export default SectionContact;
