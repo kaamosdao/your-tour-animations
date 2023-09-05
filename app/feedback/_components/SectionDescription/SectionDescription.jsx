c048a316cf2d62ac79f6fb13e0a82309c5655065
Alexander <kaamosdao@gmail.com>
2023-09-05 17:45:17 +0300

    Change app: correct layouts for secondary pages

--- app/feedback/_components/SectionDescription/SectionDescription.jsx
+++ app/feedback/_components/SectionDescription/SectionDescription.jsx
@@ -1,11 +1,24 @@
 import s from './SectionDescription.module.scss';
 
 const SectionDescription = () => (
-  <section className={s.sectionDescription}>
-    <div className={s.tagline}>
-      <h2 className={s.title}>Отзывы</h2>
-    </div>
-  </section>
+  <div className="Отзывы">
+    <section className={s.sectionDescription}>
+      <div className={s.tagline}>
+        <h2 className={s.title}>Отзывы</h2>
+        <p className={s.description}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <p className={s.description}>
+          Nostrum sed eaque eligendi aspernatur ab fugiat rerum, adipisci
+          voluptas cumque facere?
+        </p>
+        <p className={s.description}>
+          Ut facilis reiciendis fugit sed facere! Voluptates maxime minima
+          fugiat.
+        </p>
+      </div>
+    </section>
+  </div>
 );
 
 export default SectionDescription;
--- app/tours/_components/SectionDescription/SectionDescription.jsx
+++ app/tours/_components/SectionDescription/SectionDescription.jsx
@@ -1,24 +1,51 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+
 import s from './SectionDescription.module.scss';
 
-const SectionDescription = () => (
-  <section className={s.sectionDescription}>
-    <div className={s.tagline}>
-      <h2 className={s.title}>Туры</h2>
-      <p className={s.description}>
-        A robot may not injure a human being or, through inaction, allow a human
-        being to come to harm.
-      </p>
-      <p className={s.description}>
-        A robot must obey the orders given it by human beings except where such
-        orders would conflict with the First Law.
-      </p>
-      <p className={s.description}>
-        A robot must protect its own existence as long as such protection does
-        not conflict with the First or Second Law.
-      </p>
-      <p className={s.description}>(c)</p>
-    </div>
-  </section>
-);
+const SectionDescription = () => {
+  const tagline = useRef(null);
+  const q = gsap.utils.selector(tagline);
+
+  useEffect(() => {
+    const title = q('h2[class*="title"]');
+    const description = q('p[class*="description"]');
+
+    gsap.timeline().fromTo(
+      [title, description],
+      {
+        y: 1000,
+      },
+      {
+        y: 0,
+        ease: 'back.out(1.0)',
+        duration: 1,
+        stagger: 0.1,
+      }
+    );
+  }, [q]);
+
+  return (
+    <section className={s.sectionDescription}>
+      <div ref={tagline} className={s.tagline}>
+        <h2 className={s.title}>Туры</h2>
+        <p className={s.description}>
+          A robot may not injure a human being or, through inaction, allow a
+          human being to come to harm.
+        </p>
+        <p className={s.description}>
+          A robot must obey the orders given it by human beings except where
+          such orders would conflict with the First Law.
+        </p>
+        <p className={s.description}>
+          A robot must protect its own existence as long as such protection does
+          not conflict with the First or Second Law.
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default SectionDescription;
