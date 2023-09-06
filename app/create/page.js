81d6dff8926dc203501f767416aae643f60edf69
Alexander <kaamosdao@gmail.com>
2023-09-06 12:25:16 +0300

    Change app: add PageTemplate to secondary pages

--- app/create/page.js
+++ app/create/page.js
@@ -1,10 +1,15 @@
-import SectionDescription from './_components/SectionDescription/SectionDescription';
+import PageTemplate from '../_components/PageTemplate';
 
 const Tours = () => (
-  <main>
-    <h1 className="visually-hidden">Создать тур</h1>
-    <SectionDescription />
-  </main>
+  <PageTemplate
+    h1="Страница Туров"
+    h2="Туры"
+    p={[
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      'Nostrum sed eaque eligendi aspernatur ab fugiat rerum, adipisci voluptas cumque facere?',
+      'Ut facilis reiciendis fugit sed facere! Voluptates maxime minima fugiat.',
+    ]}
+  />
 );
 
 export default Tours;
--- app/tours/page.js
+++ app/tours/page.js
@@ -1,10 +1,15 @@
-import SectionDescription from './_components/SectionDescription/SectionDescription';
+import PageTemplate from '../_components/PageTemplate';
 
 const Tours = () => (
-  <main>
-    <h1 className="visually-hidden">Список туров</h1>
-    <SectionDescription />
-  </main>
+  <PageTemplate
+    h1="Страница Туров"
+    h2="Туры"
+    p={[
+      'A robot may not injure a human being or, through inaction, allow a human being to come to harm.',
+      'A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.',
+      'A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.',
+    ]}
+  />
 );
 
 export default Tours;
