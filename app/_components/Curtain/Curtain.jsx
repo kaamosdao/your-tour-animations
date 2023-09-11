caa75b978390bb3ba9f1ed1f45fcb32fd5154ab9
Alexander <kaamosdao@gmail.com>
2023-09-11 16:58:26 +0300

    Change Curtain: change transition animation

--- app/_components/Curtain/Curtain.jsx
+++ app/_components/Curtain/Curtain.jsx
@@ -4,61 +4,84 @@ import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { gsap } from 'gsap';
 
-import { setPlaying } from '@/store/slices/transitionSlice';
-import selectPlayingTransition from '@/store/selectors/transitionSelectors';
+import { setAnimation } from '@/store/slices/transitionSlice';
+import selectTransitionAnimation from '@/store/selectors/transitionSelectors';
 
 import s from './Curtain.module.scss';
+import { transitionAnimation } from '@/utils/types';
 
 const Curtain = () => {
   const curtain = useRef(null);
   const dispatch = useDispatch();
-  const playing = useSelector(selectPlayingTransition);
+  const animation = useSelector(selectTransitionAnimation);
 
   useEffect(() => {
-    if (!playing) {
+    if (!animation) {
       return;
     }
+
+    const startPoint1 =
+      animation === transitionAnimation.vertical ? '--clipblY' : '--cliptrX';
+    const startPoint2 =
+      animation === transitionAnimation.vertical ? '--clipbrY' : '--clipbrX';
+    const endPoint1 =
+      animation === transitionAnimation.vertical ? '--cliptlY' : '--cliptlX';
+    const endPoint2 =
+      animation === transitionAnimation.vertical ? '--cliptrY' : '--clipblX';
+
+    const initPoint1 =
+      animation === transitionAnimation.vertical ? '--cliptrX' : '--clipbrY';
+    const initPoint2 =
+      animation === transitionAnimation.vertical ? '--clipbrX' : '--clipblY';
 
     gsap
       .timeline({
         onComplete: () => {
-          dispatch(setPlaying(false));
+          dispatch(setAnimation(null));
         },
       })
+      .set(curtain.current, {
+        [initPoint1]: '100%',
+        [initPoint2]: '100%',
+      })
       .to(curtain.current, {
-        '--cliptr': '100%',
+        [startPoint1]: '100%',
         duration: 1,
       })
       .to(
         curtain.current,
         {
-          '--clipbr': '100%',
+          [startPoint2]: '100%',
           duration: 1,
           delay: 0.01,
         },
         '<'
       )
       .to(curtain.current, {
-        '--cliptl': '100%',
+        [endPoint1]: '100%',
         duration: 1,
       })
       .to(
         curtain.current,
         {
-          '--clipbl': '100%',
+          [endPoint2]: '100%',
           duration: 1,
           delay: 0.01,
         },
         '<'
       )
       .to(curtain.current, {
-        '--cliptr': '0%',
-        '--clipbr': '0%',
-        '--cliptl': '0%',
-        '--clipbl': '0%',
+        '--cliptrX': '0%',
+        '--clipbrX': '0%',
+        '--cliptlX': '0%',
+        '--clipblX': '0%',
+        '--cliptrY': '0%',
+        '--cliptlY': '0%',
+        '--clipbrY': '0%',
+        '--clipblY': '0%',
         duration: 0,
       });
-  }, [playing, dispatch]);
+  }, [animation, dispatch]);
 
   return <div ref={curtain} className={s.curtain} />;
 };
