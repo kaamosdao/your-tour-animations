73ab7700c4a8f863db474d0c71017565b56edd8a
Alexander <kaamosdao@gmail.com>
2023-09-21 18:45:14 +0300

    Revert "Change app: add isvisiblepage hook"
    This reverts commit b01b1955a99dcead6428a27820b93c175696b980.

--- hooks/useIsVisiblePage.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import VisiblePageContext from './context/VisiblePageContext.js';
-
-const useIsVisiblePage = () => useContext(VisiblePageContext);
-
-export default useIsVisiblePage;
--- utils/isVisible.js
+++ utils/isVisible.js
@@ -1,8 +1,9 @@
 import { transitionState } from './types';
 
+/* eslint-disable no-console */
 const isVisible = (status) => {
   const { entering, entered } = transitionState;
-
+  console.log(status);
   return !!(status === entering || status === entered);
 };
 
