50bbfeb5c58a525c13c397e9fb970c3996192e50
Alexander <kaamosdao@gmail.com>
2023-09-22 17:10:35 +0300

    Change SectionChooseTour: add scrolltrigger

--- app/_components/SectionChooseTour/TourLinks/TourLinks.jsx
+++ app/_components/SectionChooseTour/TourLinks/TourLinks.jsx
@@ -1,17 +1,77 @@
-import TourCard from '../TourCard/TourCard';
+'use client';
+
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import { tours } from '@/data';
 
+import TourCard from '../TourCard/TourCard';
+
 import s from './TourLinks.module.scss';
 
-const TourLinks = () => (
-  <ul className={s.links}>
-    {tours.map(({ name, title, price }) => (
-      <li key={name} className={s.linksItem}>
-        <TourCard name={name} title={title} price={price} />
-      </li>
-    ))}
-  </ul>
-);
+const TourLinks = () => {
+  const cardsRef = useRef(null);
+  const q = gsap.utils.selector(cardsRef);
+
+  useEffect(() => {
+    const cards = q('a');
+
+    // gsap.set(cards, {y: -700});
+
+    // ScrollTrigger.batch(cards, {
+    //   start: "30% bottom",
+    //   onEnter: (batch) =>
+    //     gsap.to(
+    //       batch,
+    //       {
+    //         y: 0,
+    //         ease: 'power3.out',
+    //         duration: 0.4,
+    //         stagger: 0.2,
+    //       }
+    //     ),
+    //   onLeaveBack: (batch) =>
+    //     gsap.to(
+    //       batch,
+    //       {
+    //         y: -700,
+    //         ease: 'power3.out',
+    //         duration: 0.4,
+    //         stagger: 0.2,
+    //       }
+    //     ),
+    // });
+
+    cards.forEach((card) => {
+      gsap.fromTo(
+        card,
+        {
+          y: -1000,
+        },
+        {
+          y: 0,
+          ease: 'power1.inOut',
+          scrollTrigger: {
+            trigger: card,
+            start: 'top bottom',
+            end: '20% center',
+            scrub: true,
+          },
+        }
+      );
+    });
+  }, [q]);
+
+  return (
+    <ul ref={cardsRef} className={s.links}>
+      {tours.map(({ name, title, price }) => (
+        <li key={name} className={s.linksItem}>
+          <TourCard name={name} title={title} price={price} />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default TourLinks;
--- app/_components/SectionChooseTour/TourNavigation/TourNavigation.jsx
+++ app/_components/SectionChooseTour/TourNavigation/TourNavigation.jsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 import classnames from 'classnames/bind';
 
 import { navigation } from '@/data';
@@ -10,18 +12,52 @@ import s from './TourNavigation.module.scss';
 
 const cn = classnames.bind(s);
 
-const TourNavigation = () => (
-  <ul className={s.nav}>
-    {navigation.map(({ name, current, link }) => (
-      <li key={name} className={s.navItem}>
-        <HoverCursor cursorType="stuck" activeLink="navLinkCurrent">
-          <a className={cn(s.navLink, { navLinkCurrent: current })} href={link}>
-            {name}
-          </a>
-        </HoverCursor>
-      </li>
-    ))}
-  </ul>
-);
+const TourNavigation = () => {
+  const navRef = useRef(null);
+  const q = gsap.utils.selector(navRef);
+
+  useEffect(() => {
+    const links = q('li');
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: navRef?.current,
+          start: 'top bottom',
+          end: '+=200',
+          toggleActions: 'restart play reverse pause',
+        },
+      })
+      .fromTo(
+        links,
+        {
+          y: -1000,
+        },
+        {
+          y: 0,
+          ease: 'power3.out',
+          duration: 0.5,
+          stagger: 0.2,
+        }
+      );
+  }, [q]);
+
+  return (
+    <ul ref={navRef} className={s.nav}>
+      {navigation.map(({ name, current, link }) => (
+        <li key={name}>
+          <HoverCursor cursorType="stuck" activeLink="navLinkCurrent">
+            <a
+              className={cn(s.navLink, { navLinkCurrent: current })}
+              href={link}
+            >
+              {name}
+            </a>
+          </HoverCursor>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default TourNavigation;
