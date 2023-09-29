cd73047355c8d384049adc3abf657b3b9f0b021f
Alexander <kaamosdao@gmail.com>
2023-09-29 17:55:05 +0300

    Change app: add pictures

--- data/pictures.js
+++ data/pictures.js
@@ -2,239 +2,369 @@ const pictures = [
   {
     name: 'itemAirplane',
     position: ['bot', 'top'],
-    desktopImg: {
-      width: 442,
-      height: 300,
-      url: '/img/photos/airplane-desktop-lg',
-    },
-    tabletImg: {
-      width: 314,
-      height: 300,
-      url: '/img/photos/airplane-desktop-md',
-    },
-    defaultImg: {
-      width: 150,
-      height: 110,
-      url: '/img/photos/airplane-mobile',
+    variants: {
+      desktopLg: {
+        width: 442,
+        height: 300,
+        url: '/img/photos/airplane-desktop-lg',
+      },
+      tabletMd: {
+        width: 314,
+        height: 300,
+        url: '/img/photos/airplane-desktop-md',
+      },
+      mobile: {
+        width: 150,
+        height: 110,
+        url: '/img/photos/airplane-mobile',
+      },
     },
     alt: 'Самолет в небе',
-    format: 'png',
+    format: 'webp',
   },
   {
     name: 'itemCanyon',
     position: ['bot', 'top'],
-    desktopImg: null,
-    tabletImg: null,
-    defaultImg: {
-      width: 442,
-      height: 301,
-      url: '/img/photos/canyon-desktop-lg',
+    variants: {
+      desktopLg: {
+        width: 442,
+        height: 301,
+        url: '/img/photos/canyon-desktop-lg',
+      },
     },
     alt: 'Палатка в каньоне',
-    format: 'png',
+    format: 'webp',
   },
   {
     name: 'itemSunset',
     position: ['bot', 'top'],
-    desktopImg: {
-      width: 442,
-      height: 300,
-      url: '/img/photos/sunset-desktop-lg',
-    },
-    tabletImg: null,
-    defaultImg: {
-      width: 314,
-      height: 301,
-      url: '/img/photos/sunset-desktop-md',
+    variants: {
+      desktopLg: {
+        width: 442,
+        height: 300,
+        url: '/img/photos/sunset-desktop-lg',
+      },
+      tabletMd: {
+        width: 314,
+        height: 301,
+        url: '/img/photos/sunset-desktop-md',
+      },
     },
     alt: 'Закат в горах',
-    format: 'png',
+    format: 'webp',
   },
   {
     name: 'itemRoad',
     position: ['bot', 'top'],
-    desktopImg: {
-      width: 442,
-      height: 300,
-      url: '/img/photos/road-desktop-lg',
-    },
-    tabletImg: {
-      width: 314,
-      height: 300,
-      url: '/img/photos/road-desktop-md',
-    },
-    defaultImg: {
-      width: 150,
-      height: 110,
-      url: '/img/photos/road-mobile',
+    variants: {
+      desktopLg: {
+        width: 442,
+        height: 300,
+        url: '/img/photos/road-desktop-lg',
+      },
+      tabletMd: {
+        width: 314,
+        height: 300,
+        url: '/img/photos/road-desktop-md',
+      },
+      mobile: {
+        width: 150,
+        height: 110,
+        url: '/img/photos/road-mobile',
+      },
     },
     alt: 'Туманная дорога в горах',
-    format: 'png',
+    format: 'webp',
   },
   {
     name: 'itemCliff',
     position: ['mid'],
-    desktopImg: {
-      width: 349,
-      height: 301,
-      url: '/img/photos/cliff-desktop-lg',
-    },
-    tabletImg: {
-      width: 231,
-      height: 301,
-      url: '/img/photos/cliff-desktop-md',
-    },
-    defaultImg: {
-      width: 99,
-      height: 110,
-      url: '/img/photos/cliff-mobile',
+    variants: {
+      desktopLg: {
+        width: 349,
+        height: 301,
+        url: '/img/photos/cliff-desktop-lg',
+      },
+      tabletMd: {
+        width: 231,
+        height: 301,
+        url: '/img/photos/cliff-desktop-md',
+      },
+      mobile: {
+        width: 99,
+        height: 110,
+        url: '/img/photos/cliff-mobile',
+      },
     },
     alt: 'Люди на утесе',
-    format: 'png',
+    format: 'webp',
   },
   {
     name: 'itemMap',
     position: ['mid'],
-    desktopImg: {
-      width: 347,
-      height: 301,
-      url: '/img/photos/map-desktop-lg',
-    },
-    tabletImg: null,
-    defaultImg: {
-      width: 231,
-      height: 301,
-      url: '/img/photos/map-desktop-md',
+    variants: {
+      desktopLg: {
+        width: 347,
+        height: 301,
+        url: '/img/photos/map-desktop-lg',
+      },
+      tabletMd: {
+        width: 231,
+        height: 301,
+        url: '/img/photos/map-desktop-md',
+      },
     },
     alt: 'Рука указывает на карту',
-    format: 'png',
+    format: 'webp',
   },
   {
     name: 'itemBridge',
     position: ['mid'],
-    desktopImg: null,
-    tabletImg: null,
-    defaultImg: {
-      width: 347,
-      height: 301,
-      url: '/img/photos/bridge-desktop-lg',
+    variants: {
+      desktopLg: {
+        width: 347,
+        height: 301,
+        url: '/img/photos/bridge-desktop-lg',
+      },
     },
     alt: 'Мост в лесу',
-    format: 'png',
+    format: 'webp',
   },
   {
     name: 'itemCar',
     position: ['mid'],
-    desktopImg: {
-      width: 347,
-      height: 301,
-      url: '/img/photos/car-desktop-lg',
-    },
-    tabletImg: {
-      width: 231,
-      height: 301,
-      url: '/img/photos/car-desktop-md',
-    },
-    defaultImg: {
-      width: 99,
-      height: 110,
-      url: '/img/photos/car-mobile',
+    variants: {
+      desktopLg: {
+        width: 347,
+        height: 301,
+        url: '/img/photos/car-desktop-lg',
+      },
+      tabletMd: {
+        width: 231,
+        height: 301,
+        url: '/img/photos/car-desktop-md',
+      },
+      mobile: {
+        width: 99,
+        height: 110,
+        url: '/img/photos/car-mobile',
+      },
     },
     alt: 'Машина с палаткой у воды',
-    format: 'png',
+    format: 'webp',
   },
   {
     name: 'itemStreet',
     position: ['mid'],
-    desktopImg: {
-      width: 347,
-      height: 301,
-      url: '/img/photos/street-desktop-lg',
-    },
-    tabletImg: {
-      width: 231,
-      height: 301,
-      url: '/img/photos/street-desktop-md',
-    },
-    defaultImg: {
-      width: 99,
-      height: 110,
-      url: '/img/photos/street-mobile',
+    variants: {
+      desktopLg: {
+        width: 347,
+        height: 301,
+        url: '/img/photos/street-desktop-lg',
+      },
+      tabletMd: {
+        width: 231,
+        height: 301,
+        url: '/img/photos/street-desktop-md',
+      },
+      mobile: {
+        width: 99,
+        height: 110,
+        url: '/img/photos/street-mobile',
+      },
     },
     alt: 'Пустая улица',
-    format: 'png',
+    format: 'webp',
+  },
+  {
+    name: 'itemBeach',
+    position: ['mid'],
+    variants: {
+      desktopLg: {
+        width: 347,
+        height: 301,
+        url: '/img/photos/beach-desktop-lg',
+      },
+      tabletMd: {
+        width: 231,
+        height: 301,
+        url: '/img/photos/beach-desktop-md',
+      },
+      mobile: {
+        width: 99,
+        height: 110,
+        url: '/img/photos/beach-mobile',
+      },
+    },
+    alt: 'Пляж и горизонт',
+    format: 'webp',
+  },
+  {
+    name: 'itemTrees',
+    position: ['mid'],
+    variants: {
+      desktopLg: {
+        width: 347,
+        height: 301,
+        url: '/img/photos/trees-desktop-lg',
+      },
+      tabletMd: {
+        width: 231,
+        height: 301,
+        url: '/img/photos/trees-desktop-md',
+      },
+    },
+    alt: 'Лес',
+    format: 'webp',
+  },
+  {
+    name: 'itemSea',
+    position: ['mid'],
+    variants: {
+      desktopLg: {
+        width: 347,
+        height: 301,
+        url: '/img/photos/sea-desktop-lg',
+      },
+      tabletMd: {
+        width: 231,
+        height: 301,
+        url: '/img/photos/sea-desktop-md',
+      },
+    },
+    alt: 'Море',
+    format: 'webp',
+  },
+  {
+    name: 'itemWaves',
+    position: ['mid'],
+    variants: {
+      desktopLg: {
+        width: 347,
+        height: 301,
+        url: '/img/photos/waves-desktop-lg',
+      },
+      tabletMd: {
+        width: 231,
+        height: 301,
+        url: '/img/photos/waves-desktop-md',
+      },
+    },
+    alt: 'Волны',
+    format: 'webp',
+  },
+  {
+    name: 'itemLegs',
+    position: ['mid'],
+    variants: {
+      desktopLg: {
+        width: 347,
+        height: 301,
+        url: '/img/photos/legs-desktop-lg',
+      },
+      tabletMd: {
+        width: 231,
+        height: 301,
+        url: '/img/photos/legs-desktop-md',
+      },
+    },
+    alt: 'Ноги',
+    format: 'webp',
+  },
+  {
+    name: 'itemIreland',
+    position: ['mid'],
+    variants: {
+      desktopLg: {
+        width: 347,
+        height: 301,
+        url: '/img/photos/ireland-desktop-lg',
+      },
+      tabletMd: {
+        width: 231,
+        height: 301,
+        url: '/img/photos/ireland-desktop-md',
+      },
+    },
+    alt: 'Ирландия',
+    format: 'webp',
   },
   {
     name: 'itemHouses',
     position: ['bot', 'top'],
-    desktopImg: {
-      width: 442,
-      height: 300,
-      url: '/img/photos/houses-desktop-lg',
-    },
-    tabletImg: {
-      width: 314,
-      height: 300,
-      url: '/img/photos/houses-desktop-md',
-    },
-    defaultImg: {
-      width: 154,
-      height: 110,
-      url: '/img/photos/houses-mobile',
+    variants: {
+      desktopLg: {
+        width: 442,
+        height: 300,
+        url: '/img/photos/houses-desktop-lg',
+      },
+      tabletMd: {
+        width: 314,
+        height: 300,
+        url: '/img/photos/houses-desktop-md',
+      },
+      mobile: {
+        width: 154,
+        height: 110,
+        url: '/img/photos/houses-mobile',
+      },
     },
     alt: 'Дома на улице',
-    format: 'png',
+    format: 'webp',
   },
   {
     name: 'itemLake',
     position: ['bot', 'top'],
-    desktopImg: {
-      width: 442,
-      height: 300,
-      url: '/img/photos/lake-desktop-lg',
-    },
-    tabletImg: {
-      width: 314,
-      height: 300,
-      url: '/img/photos/lake-desktop-md',
-    },
-    defaultImg: {
-      width: 154,
-      height: 110,
-      url: '/img/photos/lake-mobile',
+    variants: {
+      desktopLg: {
+        width: 442,
+        height: 300,
+        url: '/img/photos/lake-desktop-lg',
+      },
+      tabletMd: {
+        width: 314,
+        height: 300,
+        url: '/img/photos/lake-desktop-md',
+      },
+      mobile: {
+        width: 154,
+        height: 110,
+        url: '/img/photos/lake-mobile',
+      },
     },
     alt: 'Озеро в горах',
-    format: 'png',
+    format: 'webp',
   },
   {
     name: 'itemTown',
     position: ['bot', 'top'],
-    desktopImg: {
-      width: 442,
-      height: 300,
-      url: '/img/photos/town-desktop-lg',
-    },
-    tabletImg: null,
-    defaultImg: {
-      width: 314,
-      height: 300,
-      url: '/img/photos/town-desktop-md',
+    variants: {
+      desktopLg: {
+        width: 442,
+        height: 300,
+        url: '/img/photos/town-desktop-lg',
+      },
+      tabletMd: {
+        width: 314,
+        height: 300,
+        url: '/img/photos/town-desktop-md',
+      },
     },
     alt: 'Оживленная улица',
-    format: 'png',
+    format: 'webp',
   },
   {
     name: 'itemSupboard',
     position: ['bot', 'top'],
-    desktopImg: null,
-    tabletImg: null,
-    defaultImg: {
-      width: 442,
-      height: 300,
-      url: '/img/photos/supboard-desktop-lg',
+    variants: {
+      desktopLg: {
+        width: 442,
+        height: 300,
+        url: '/img/photos/supboard-desktop-lg',
+      },
     },
     alt: 'SUP борд на воде',
-    format: 'png',
+    format: 'webp',
   },
 ];
 
