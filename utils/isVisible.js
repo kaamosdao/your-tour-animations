131bcfb1b0a975c529a41097c335e2ff4afe34db
Alexander <kaamosdao@gmail.com>
2023-10-12 17:06:01 +0300

    Refactor isVisible: add spaces

--- utils/isVisible.js
+++ utils/isVisible.js
@@ -2,12 +2,15 @@ import { transitionState } from './types';
 
 const isVisible = (status) => {
   const { entering, entered, exiting, exited } = transitionState;
+
   if (status === entering || status === entered) {
     return true;
   }
+
   if (status === exiting || status === exited) {
     return false;
   }
+
   return undefined;
 };
 
