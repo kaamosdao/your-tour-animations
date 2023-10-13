522f047a351ba96be29715cea9b1f09f8cfc1513
Alexander <kaamosdao@gmail.com>
2023-10-13 15:55:59 +0300

    Refactor IsVisible: correct isvisible

--- utils/isVisible.js
+++ utils/isVisible.js
@@ -1,17 +1,9 @@
 import { transitionState } from './types';
 
 const isVisible = (status) => {
-  const { entering, entered, exiting, exited } = transitionState;
+  const { entering, entered } = transitionState;
 
-  if (status === entering || status === entered) {
-    return true;
-  }
-
-  if (status === exiting || status === exited) {
-    return false;
-  }
-
-  return undefined;
+  return status === entering || status === entered;
 };
 
 export default isVisible;
