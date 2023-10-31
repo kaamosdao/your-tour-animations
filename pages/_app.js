b4eef565d7391a511b20fe2d2cb7bf6bcd89e396
Alexander <kaamosdao@gmail.com>
2023-10-31 21:24:28 +0300

    Change app: change from router to usePathname

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,28 @@
+import { usePathname } from 'next/navigation';
+import { PrismicPreview } from '@prismicio/next';
+
+import { repositoryName } from '@/prismicio';
+
 import RootLayout from '@/components/RootLayout';
 import { ReactSwitchTransition } from '@/components/TransitionGroup';
 
 import '@/styles/style.scss';
 
-export default function App({ Component, pageProps, router }) {
+export default function App({ Component, pageProps }) {
+  const pathName = usePathname();
+
   return (
-    <RootLayout>
-      <ReactSwitchTransition
-        transitionKey={router.route}
-        timeout={{ exit: 900 }}
-        mode="out-in"
-      >
-        <Component {...pageProps} />
-      </ReactSwitchTransition>
-    </RootLayout>
+    <>
+      <RootLayout>
+        <ReactSwitchTransition
+          transitionKey={pathName}
+          timeout={{ exit: 900 }}
+          mode="out-in"
+        >
+          <Component {...pageProps} />
+        </ReactSwitchTransition>
+      </RootLayout>
+      <PrismicPreview repositoryName={repositoryName} />
+    </>
   );
 }
