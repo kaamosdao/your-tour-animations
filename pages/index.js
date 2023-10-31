e3fcdc06d547c38dbb6b7698cf20b7885559d006
Alexander <kaamosdao@gmail.com>
2023-10-31 21:25:44 +0300

    Change app: add prismic homepage

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,53 @@
-import SectionChooseTour from '@/components/SectionChooseTour';
-import SectionConstructTour from '@/components/SectionConstructTour';
-import SectionContact from '@/components/SectionContact';
-import SectionDescription from '@/components/SectionDescription';
-import SectionFeedback from '@/components/SectionFeedback';
-import SectionHistories from '@/components/SectionHistories';
-import SectionPhotos from '@/components/SectionPhotos';
+// import SectionChooseTour from '@/components/SectionChooseTour';
+// import SectionConstructTour from '@/components/SectionConstructTour';
+// import SectionContact from '@/components/SectionContact';
+// import SectionDescription from '@/components/SectionDescription';
+// import SectionFeedback from '@/components/SectionFeedback';
+// import SectionHistories from '@/components/SectionHistories';
+// import SectionPhotos from '@/components/SectionPhotos';
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <main>
+//       <h1 className="visually-hidden">Выбери свой тур c YourTour</h1>
+//       <SectionDescription />
+//       <SectionChooseTour />
+//       <SectionConstructTour />
+//       <SectionFeedback />
+//       <SectionPhotos />
+//       <SectionHistories />
+//       <SectionContact />
+//     </main>
+//   );
+// }
+
+import Head from 'next/head';
+import { isFilled } from '@prismicio/client';
+import { SliceZone } from '@prismicio/react';
+
+import { components } from '@/slices';
+import { createClient } from '@/prismicio';
+
+export default function Page({ page }) {
   return (
-    <main>
-      <h1 className="visually-hidden">Выбери свой тур c YourTour</h1>
-      <SectionDescription />
-      <SectionChooseTour />
-      <SectionConstructTour />
-      <SectionFeedback />
-      <SectionPhotos />
-      <SectionHistories />
-      <SectionContact />
-    </main>
+    <>
+      <Head>
+        <title>{page.data.meta_title}</title>
+        {isFilled.keyText(page.data.meta_description) ? (
+          <meta name="description" content={page.data.meta_description} />
+        ) : null}
+      </Head>
+      <SliceZone slices={page.data.slices} components={components} />
+    </>
   );
+}
+
+export async function getStaticProps({ previewData }) {
+  const client = createClient({ previewData });
+
+  const page = await client.getSingle('homepage');
+
+  return {
+    props: { page },
+  };
 }
