0aaa6b787e692969b46aff9cc87d3fd818ed33fd
Alexander <kaamosdao@gmail.com>
2023-11-09 17:25:52 +0300

    Change Footer: add socials from data

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,7 +3,8 @@
 import { useEffect, useState } from 'react';
 import { PrismicNextLink } from '@prismicio/next';
 import { createClient } from '@/prismicio';
-import { FacebookIcon, InstagramIcon, VkIcon } from '@/components/Footer/Icons';
+
+import socials from '@/data/socials';
 
 import HoverCursor from '../CustomCursor/HoverCursor';
 
@@ -26,36 +27,16 @@ const Footer = () => {
       <div className={s.container}>
         <p className={s.text}>Наши социальные сети</p>
         <ul className={s.socials}>
-          <li className={s.socialsItem}>
-            <InstagramIcon />
-            <HoverCursor cursorType="pulse">
-              <PrismicNextLink
-                field={data?.instagram}
-                className={s.socialsLink}
-              >
-                instagram
-              </PrismicNextLink>
-            </HoverCursor>
-          </li>
-          <li className={s.socialsItem}>
-            <FacebookIcon />
-            <HoverCursor cursorType="pulse">
-              <PrismicNextLink field={data?.facebook} className={s.socialsLink}>
-                facebook
-              </PrismicNextLink>
-            </HoverCursor>
-          </li>
-          <li className={s.socialsItem}>
-            <VkIcon />
-            <HoverCursor cursorType="pulse">
-              <PrismicNextLink
-                field={data?.vkontakte}
-                className={s.socialsLink}
-              >
-                vkontakte
-              </PrismicNextLink>
-            </HoverCursor>
-          </li>
+          {socials.map(({ name, icon }) => (
+            <li key={name} className={s.socialsItem}>
+              {icon}
+              <HoverCursor cursorType="pulse">
+                <PrismicNextLink field={data?.[name]} className={s.socialsLink}>
+                  {name}
+                </PrismicNextLink>
+              </HoverCursor>
+            </li>
+          ))}
         </ul>
       </div>
     </footer>
