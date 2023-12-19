04b227e46482ee6bad12a4ff86d5e0389d9643e3
Alexander <kaamosdao@gmail.com>
2023-12-19 14:06:48 +0300

    Change DescriptionSecondary: remove refresh scrolltrigger

--- components/DescriptionSecondary/DescriptionSecondary.jsx
+++ components/DescriptionSecondary/DescriptionSecondary.jsx
@@ -2,8 +2,6 @@
 
 /* eslint-disable react/no-array-index-key */
 
-import { useEffect } from 'react';
-import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 import { PrismicRichText } from '@prismicio/react';
 import cn from 'classnames';
 
@@ -31,10 +29,6 @@ const DescriptionSecondary = ({ slice }) => {
   const isVisible = useTransition();
 
   const [refs, addRef] = useArrayRef();
-
-  useEffect(() => {
-    ScrollTrigger.refresh();
-  }, []);
 
   return (
     <section
