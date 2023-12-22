91b1327af53d0197575d9ae5326138102de101ce
Alexander <kaamosdao@gmail.com>
2023-12-22 20:38:52 +0300

    Change hooks: add useAppDispatch, useAppSelector hooks

--- hooks/index.js
+++ hooks/index.js
@@ -2,3 +2,5 @@ export { default as useTransition } from './useTransition.js';
 export { default as useArrayRef } from './useArrayRef.js';
 export { default as useDevicePixelRatio } from './useDevicePixelRatio.js';
 export { default as useWiderThanTabletLg } from './useWiderThanTabletLg.js';
+export { default as useAppDispatch } from './useAppDispatch.ts';
+export { default as useAppSelector } from './useAppSelector.ts';
