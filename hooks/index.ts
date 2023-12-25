110fe2ea4431d827570734976c9c3ffe5b68cc4e
Alexander <kaamosdao@gmail.com>
2023-12-25 18:49:39 +0300

    Change app: change index extensions to ts

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,6 +1,6 @@
 export { default as useTransition } from './useTransition.js';
-export { default as useArrayRef } from './useArrayRef.ts';
+export { default as useArrayRef } from './useArrayRef';
 export { default as useDevicePixelRatio } from './useDevicePixelRatio.js';
 export { default as useWiderThanTabletLg } from './useWiderThanTabletLg.js';
-export { default as useAppDispatch } from './useAppDispatch.ts';
-export { default as useAppSelector } from './useAppSelector.ts';
+export { default as useAppDispatch } from './useAppDispatch';
+export { default as useAppSelector } from './useAppSelector';
